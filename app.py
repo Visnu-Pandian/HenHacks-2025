19cb2a8ae9549c99ddd931ab4d06ceb135198665
Visnu S Pandian <visnupandian1@gmail.com>
2025-03-01 14:39:45 -0500

    commit to fix merge issues

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import icalendar
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -27,7 +28,23 @@ def upload_file():
 
 @app.route('/results/<filename>')
 def results(filename):
-    return render_template('results.html', filename=filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    with open(file_path, 'rb') as file:
+        gcal = icalendar.Calendar.from_ical(file.read())
+    
+    events = []
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            event = {
+                'summary': component.get('summary'),
+                'dtstart': component.get('dtstart').dt,
+                'dtend': component.get('dtend').dt,
+                'location': component.get('location'),
+                'description': component.get('description')
+            }
+            events.append(event)
+    
+    return render_template('results.html', filename=filename, events=events)
 
 if __name__ == '__main__':
     app.run(debug=True)
