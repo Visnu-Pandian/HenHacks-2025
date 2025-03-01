7eb6d493d8e67201b7c4e1f515739c1f2d9dfd3e
Visnu S Pandian <visnupandian1@gmail.com>
2025-02-28 23:42:50 -0500

    Fixed file input.

--- frontend/src/app.js
+++
@@ -1,6 +0,0 @@
-const FileUpload = require('./components/FileUpload');
-
-document.addEventListener('DOMContentLoaded', () => {
-    const fileUpload = new FileUpload();
-    fileUpload.init();
-});
